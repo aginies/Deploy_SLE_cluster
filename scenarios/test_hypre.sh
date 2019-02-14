1edff4968c3a5a57e743e7510d6611c7892d84fb
Antoine Ginies <aginies@suse.com>
2019-02-14 11:04:45 +0100

    various fix to get the testsuite usable

--- scenarios/test_hypre.sh
+++ scenarios/test_hypre.sh
@@ -17,7 +17,30 @@ check_load_config_file other
 prepare_hypre() {
     echo $I "############ START prepare_hypre" $O
     echo $I "-Install hypre on node ${NODENAME}1" $O
-    exec_on_node ${NODENAME}1 "zypper in -y hypre-gnu-hpc* superlu_5_2_1-gnu-hpc-devel hypre-gnu-openmpi2-hpc-devel blas-devel"
+    exec_on_node ${NODENAME}1 "zypper in -y superlu_5_2_1-gnu-hpc-devel hypre-gnu-openmpi2-hpc-devel blas-devel"
+}
+
+BSCRIPTNAME=build_hypre.sh
+build_hypre() {
+    echo $I "############ START build_hypre" $O
+    cat > ${BSCRIPTNAME} <<EOF
+#!/bin/sh
+EXAMPLED=hypre_2_15_1-gnu-openmpi2-hpc-examples
+rm -rf ~/\${EXAMPLED}
+cp -a /usr/share/doc/packages/\${EXAMPLED} ~/\${EXAMPLED}
+module available
+module load gnu
+module load openblas
+module load superlu
+module load hypre
+cd ~/\${EXAMPLED}/examples
+#mpicc  -g -Wall -I/usr/lib/hpc/gnu7/openmpi2/hypre/2.15.1/include -DHAVE_CONFIG_H -DHYPRE_TIMING -c ex1.c
+#mpicc -o ex1 ex1.o -L/usr/lib/hpc/gnu7/superlu/5.2.1/lib64/ -L/usr/lib/hpc/gnu7/openmpi2/hypre/2.15.1/lib64 -lHYPRE -lm -lstdc++ -lblas -llapack -lsuperlu
+make -j 4
+EOF
+    scp_on_node ${BSCRIPTNAME} "test@${NODENAME}1:~/"
+    exec_on_node test@${NODENAME}1 "sh ${BSCRIPTNAME}"
+    rm ${BSCRIPTNAME}
 }
 
 RSCRIPTNAME=run_hypre.sh
@@ -25,18 +48,35 @@ run_hypre() {
     echo $I "############ START run_hypre" $O
     cat > ${RSCRIPTNAME} <<EOF
 #!/bin/sh
-EXAMPLED=hypre-examples
-cp -a /usr/share/doc/packages/${EXAMPLED} ~/${EXAMPLED}
-cd ~/${EXAMPLED}
-module available
+EXAMPLED=hypre_2_15_1-gnu-openmpi2-hpc-examples
+cd  ~/\${EXAMPLED}/examples
 module load gnu
-module load blas
+module load openblas
 module load superlu
 module load hypre
-#mpicc  -g -Wall -I/usr/lib/hpc/gnu7/openmpi2/hypre/2.15.1/include -DHAVE_CONFIG_H -DHYPRE_TIMING -c ex1.c
-#mpicc -o ex1 ex1.o -L/usr/lib/hpc/gnu7/superlu/5.2.1/lib64/ -L/usr/lib/hpc/gnu7/openmpi2/hypre/2.15.1/lib64 -lHYPRE -lm -lstdc++ -lblas -llapack -lsuperlu
-make
-#mpirun -np 6 ex13 -n 10
+mpirun -np 4 ex10 -n 120 -solver 2
+mpirun -np 4 ex11
+mpirun -np 2 ex12 -pfmg
+mpirun -np 2 ex12f
+mpirun -np 6 ex13 -n 10
+mpirun -np 6 ex14 -n 10
+mpirun -np 8 ex15big -n 10
+mpirun -np 8 ex15 -n 10
+mpirun -np 4 ex16 -n 10
+mpirun -np 16 ex17 -n 10
+mpirun -np 16 ex18 -n 4
+mpirun -np 16 ex18comp -n 4
+mpirun -np 2 ex1
+mpirun -np 2 ex2
+mpirun -np 16 ex3 -n 33 -solver 0 -v 1 1
+mpirun -np 16 ex4 -n 33 -solver 10 -K 3 -B 0 -C 1 -U0 2 -F 4
+mpirun -np 4 ex5big
+mpirun -np 4 ex5
+mpirun -np 4 ex5f
+mpirun -np 2 ex6
+mpirun -np 16 ex7 -n 33 -solver 10 -K 3 -B 0 -C 1 -U0 2 -F 4
+mpirun -np 2 ex8
+mpirun -np 16 ex9 -n 33 -solver 0 -v 1 1
 EOF
     scp_on_node ${RSCRIPTNAME} "test@${NODENAME}1:~/"
     exec_on_node test@${NODENAME}1 "sh ${RSCRIPTNAME}"
@@ -45,6 +85,7 @@ EOF
 
 clean_hypre() {
     echo $I "############ START clean_hypre" $O
+    exec_on_node test@${NODENAME}1 "rm -f ${BSCRIPTNAME}"
     exec_on_node test@${NODENAME}1 "rm -f ${RSCRIPTNAME}"
 }
 
@@ -67,6 +108,9 @@ case $1 in
     hypre)
 	prepare_hypre
 	;;
+    build)
+	build_hypre
+	;;
     run)
 	run_hypre
 	;;
@@ -75,15 +119,19 @@ case $1 in
 	;;
     all)
 	prepare_hypre
+	build_hypre
 	run_hypre
 	clean_hypre
 	;;
     *)
 	echo "
-usage of $0 {hypre|run|clean|all}
+usage of $0 {hypre|build|run|clean|all}
 
  hypre 
 	install hypre on node1
+
+ build
+	build the hypre examples
 
  run
 	run the test 
